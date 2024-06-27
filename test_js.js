194b6f3dc003b467e2c50ea82f2bf2e91a18f818
VasiliKavaleu <vasiliy.kavaleu@gmail.com>
2024-06-27 12:03:07 +0300

    Create test_js.js

---
+++ test_js.js
@@ -0,0 +1,7 @@
+function testDetailProductViewEvent() {
+  console.log('Test product view event', productViewd)
+};
+
+function testPurchaseEvent() {
+  console.log('Test purchase event', transactionItems)
+};
